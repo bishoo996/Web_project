af896f9645722df3b66c7086de4e3ee75a9e83cb
AbdelrahmanSakr1 <dodahel@gmail.com>
2026-06-07 23:45:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/web_projectDB')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Could not connect to MongoDB', err));
 
