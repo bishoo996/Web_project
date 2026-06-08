164d34a1b543adf8c7e4d9d7d9611ee78eaf85e6
youssefmo3tz <youssefmoatazislam@gmail.com>
2026-06-09 01:56:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default || require('connect-mongo');
 
 const apiRoutes = require('./routes/api');
 const viewRoutes = require('./routes/views');
